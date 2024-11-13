974bd19d3ce2ff032d33e3a9b5ac70282aa8558f
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-11-13 16:46:28 +0900

    init: initial commit

---
+++ src/main/java/com/dear_tree/dear_tree/DearTreeApplication.java
@@ -0,0 +1,13 @@
+package com.dear_tree.dear_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dear_tree/dear_tree/DearTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dear_tree.dear_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
