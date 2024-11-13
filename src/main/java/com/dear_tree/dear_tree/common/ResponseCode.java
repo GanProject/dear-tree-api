2a7b482431cae1e0b6d68ee7c5462d4edcc40f98
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-11-14 00:34:49 +0900

    chore: Add ResponseCode

---
+++ src/main/java/com/dear_tree/dear_tree/common/ResponseCode.java
@@ -0,0 +1,12 @@
+package com.dear_tree.dear_tree.common;
+
+public interface ResponseCode {
+    //HTTP Status 200
+    String SUCCESS = "SU";
+
+    //HTTP Status 401
+    String AUTHORIZATION_FAIL = "AF";
+
+    // HTTP Status 500
+    String DATABASE_ERROR = "DBE";
+}
